5c03349461cefa72c42db9668aa6ba060fc819df
pervin qurbanlı <pervinqurbanli@pervins-MacBook-Pro.local>
2022-06-25 23:06:16 +0400

    heraku deploy test4

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/career'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/career/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/career/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
